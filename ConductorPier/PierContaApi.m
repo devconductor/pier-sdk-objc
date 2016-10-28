d935f30bb566ad05aa345b99ad942ba15d0a5593
Filipe Germano <filipe.germano@conductor.com.br>
2016-10-28 17:16:44 -0300

    SPL002 - Revisando recursos para bliqueio e desbloqueio de cartão.

---
+++ ConductorPier/PierContaApi.m
@@ -0,0 +1,306 @@
+#import "PierContaApi.h"
+#import "PierQueryParamCollection.h"
+#import "PierConta.h"
+
+
+@interface PierContaApi ()
+    @property (readwrite, nonatomic, strong) NSMutableDictionary *defaultHeaders;
+@end
+
+@implementation PierContaApi
+
+static PierContaApi* singletonAPI = nil;
+
+#pragma mark - Initialize methods
+
+- (id) init {
+    self = [super init];
+    if (self) {
+        PierConfiguration *config = [PierConfiguration sharedConfig];
+        if (config.apiClient == nil) {
+            config.apiClient = [[PierApiClient alloc] init];
+        }
+        self.apiClient = config.apiClient;
+        self.defaultHeaders = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+- (id) initWithApiClient:(PierApiClient *)apiClient {
+    self = [super init];
+    if (self) {
+        self.apiClient = apiClient;
+        self.defaultHeaders = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+#pragma mark -
+
++(PierContaApi*) apiWithHeader:(NSString*)headerValue key:(NSString*)key {
+
+    if (singletonAPI == nil) {
+        singletonAPI = [[PierContaApi alloc] init];
+        [singletonAPI addHeader:headerValue forKey:key];
+    }
+    return singletonAPI;
+}
+
++(PierContaApi*) sharedAPI {
+
+    if (singletonAPI == nil) {
+        singletonAPI = [[PierContaApi alloc] init];
+    }
+    return singletonAPI;
+}
+
+-(void) addHeader:(NSString*)value forKey:(NSString*)key {
+    [self.defaultHeaders setValue:value forKey:key];
+}
+
+-(void) setHeaderValue:(NSString*) value
+           forKey:(NSString*)key {
+    [self.defaultHeaders setValue:value forKey:key];
+}
+
+-(unsigned long) requestQueueSize {
+    return [PierApiClient requestQueueSize];
+}
+
+#pragma mark - Api Methods
+
+///
+/// Apresenta dados de uma determinada conta
+/// Este m\u00C3\u00A9todo permite consultar dados de uma determinada conta a partir de seu codigo de identifica\u00C3\u00A7\u00C3\u00A3o (id).
+///  @param idConta C\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o da conta (id). 
+///
+///  @returns PierConta*
+///
+-(NSNumber*) consultarUsingGET1WithIdConta: (NSNumber*) idConta
+    completionHandler: (void (^)(PierConta* output, NSError* error)) handler {
+
+    
+    // verify the required parameter 'idConta' is set
+    if (idConta == nil) {
+        [NSException raise:@"Invalid parameter" format:@"Missing the required parameter `idConta` when calling `consultarUsingGET1`"];
+    }
+    
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/api/contas/{id_conta}"];
+
+    // remove format in URL if needed
+    if ([resourcePath rangeOfString:@".{format}"].location != NSNotFound) {
+        [resourcePath replaceCharactersInRange: [resourcePath rangeOfString:@".{format}"] withString:@".json"];
+    }
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+    if (idConta != nil) {
+        pathParams[@"id_conta"] = idConta;
+    }
+    
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.defaultHeaders];
+
+    
+
+    // HTTP header `Accept`
+    headerParams[@"Accept"] = [PierApiClient selectHeaderAccept:@[@"application/json"]];
+    if ([headerParams[@"Accept"] length] == 0) {
+        [headerParams removeObjectForKey:@"Accept"];
+    }
+
+    // response content type
+    NSString *responseContentType;
+    if ([headerParams objectForKey:@"Accept"]) {
+        responseContentType = [headerParams[@"Accept"] componentsSeparatedByString:@", "][0];
+    }
+    else {
+        responseContentType = @"";
+    }
+
+    // request content type
+    NSString *requestContentType = [PierApiClient selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[@"access_token"];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    
+    
+    
+
+    
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"GET"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: @"PierConta*"
+                           completionBlock: ^(id data, NSError *error) {
+                               handler((PierConta*)data, error);
+                           }
+          ];
+}
+
+///
+/// Lista contas existentes na base de dados do Emissor.
+/// Este m\u00C3\u00A9todo permite listar contas existentes na base de dados do Emissor.
+///  @param _id C\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o de conta (id). (optional)
+///
+///  @param idProduto C\u00C3\u00B3digo de identifica\u00C3\u00A7\u00C3\u00A3o do produto ao qual a conta faz parte. (id). (optional)
+///
+///  @param idOrigemComercial C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o da Origem Comercial (id) que deu origem a Conta. (optional)
+///
+///  @param idPessoa C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o da Pessoa Titular da Conta (id). (optional)
+///
+///  @param idStatusConta C\u00C3\u00B3digo de Identifica\u00C3\u00A7\u00C3\u00A3o do Produto a qual o cart\u00C3\u00A3o pertence (id). (optional)
+///
+///  @param diaVencimento Apresenta o dia de vencimento. (optional)
+///
+///  @param melhorDiaCompra Apresenta o melhor dia de compra. (optional)
+///
+///  @param dataStatusConta Apresenta a data em que o idStatusConta atual fora atribu\u00C3\u00ADdo para ela. (optional)
+///
+///  @param dataCadastro Apresenta a data em que o cart\u00C3\u00A3o foi gerado. (optional)
+///
+///  @param page P\u00C3\u00A1gina solicitada (Default = 0) (optional)
+///
+///  @param limit Limite de elementos por solicita\u00C3\u00A7\u00C3\u00A3o (Default = 100, Max = 100) (optional)
+///
+///  @returns PierConta*
+///
+-(NSNumber*) listarUsingGET1WithId: (NSNumber*) _id
+    idProduto: (NSNumber*) idProduto
+    idOrigemComercial: (NSNumber*) idOrigemComercial
+    idPessoa: (NSNumber*) idPessoa
+    idStatusConta: (NSNumber*) idStatusConta
+    diaVencimento: (NSNumber*) diaVencimento
+    melhorDiaCompra: (NSNumber*) melhorDiaCompra
+    dataStatusConta: (NSDate*) dataStatusConta
+    dataCadastro: (NSDate*) dataCadastro
+    page: (NSNumber*) page
+    limit: (NSNumber*) limit
+    completionHandler: (void (^)(PierConta* output, NSError* error)) handler {
+
+    
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/api/contas"];
+
+    // remove format in URL if needed
+    if ([resourcePath rangeOfString:@".{format}"].location != NSNotFound) {
+        [resourcePath replaceCharactersInRange: [resourcePath rangeOfString:@".{format}"] withString:@".json"];
+    }
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+    
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    if (_id != nil) {
+        
+        queryParams[@"id"] = _id;
+    }
+    if (idProduto != nil) {
+        
+        queryParams[@"idProduto"] = idProduto;
+    }
+    if (idOrigemComercial != nil) {
+        
+        queryParams[@"idOrigemComercial"] = idOrigemComercial;
+    }
+    if (idPessoa != nil) {
+        
+        queryParams[@"idPessoa"] = idPessoa;
+    }
+    if (idStatusConta != nil) {
+        
+        queryParams[@"idStatusConta"] = idStatusConta;
+    }
+    if (diaVencimento != nil) {
+        
+        queryParams[@"diaVencimento"] = diaVencimento;
+    }
+    if (melhorDiaCompra != nil) {
+        
+        queryParams[@"melhorDiaCompra"] = melhorDiaCompra;
+    }
+    if (dataStatusConta != nil) {
+        
+        queryParams[@"dataStatusConta"] = dataStatusConta;
+    }
+    if (dataCadastro != nil) {
+        
+        queryParams[@"dataCadastro"] = dataCadastro;
+    }
+    if (page != nil) {
+        
+        queryParams[@"page"] = page;
+    }
+    if (limit != nil) {
+        
+        queryParams[@"limit"] = limit;
+    }
+    
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.defaultHeaders];
+
+    
+
+    // HTTP header `Accept`
+    headerParams[@"Accept"] = [PierApiClient selectHeaderAccept:@[@"application/json"]];
+    if ([headerParams[@"Accept"] length] == 0) {
+        [headerParams removeObjectForKey:@"Accept"];
+    }
+
+    // response content type
+    NSString *responseContentType;
+    if ([headerParams objectForKey:@"Accept"]) {
+        responseContentType = [headerParams[@"Accept"] componentsSeparatedByString:@", "][0];
+    }
+    else {
+        responseContentType = @"";
+    }
+
+    // request content type
+    NSString *requestContentType = [PierApiClient selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[@"access_token"];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    
+    
+    
+
+    
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"GET"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: @"PierConta*"
+                           completionBlock: ^(id data, NSError *error) {
+                               handler((PierConta*)data, error);
+                           }
+          ];
+}
+
+
+
+@end
